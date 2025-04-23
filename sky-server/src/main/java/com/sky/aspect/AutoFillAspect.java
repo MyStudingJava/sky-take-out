5ca5589d4db97a6ff6d1c7648b0ca55c43929e42
HQYYK <1850115720@qq.com>
2025-04-23 10:59:06 +0800

    feat(sky-server): 实现公共字段自动填充功能
    - 新增 AutoFillAspect 切面类，用于自动填充公共字段
    - 在 CategoryMapper 和 EmployeeMapper 中添加 AutoFill 注解
    - 修改 CategoryServiceImpl 和 EmployeeServiceImpl，移除手动填充公共字段的代码
    - 新增 OperationType 枚举和 AutoFillConstant 常量类

--- sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -1,12 +1,18 @@
 package com.sky.aspect;
 
 import com.sky.annotation.AutoFill;
+import com.sky.constant.AutoFillConstant;
+import com.sky.context.BaseContext;
+import com.sky.enumeration.OperationType;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 /**
  * 自定义切面,实现自动填充功能
@@ -31,5 +37,44 @@ public class AutoFillAspect {
     @Before("autoFillPointCut()") // 指定切入点,就是方法名称
     public void autoFill(JoinPoint joinPoint){ // JoinPoint表示当前切面方法
         log.info("开始进行公共字段自动填充...");
+
+        // 1. 获取到当前被拦截的方法上的数据库操作类型
+        // Signature signature = joinPoint.getSignature();
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature(); // 方法签名转型
+        AutoFill autoFill = signature.getMethod().getAnnotation(AutoFill.class); // 获得方法上的注解对象
+        OperationType operationType = autoFill.value(); // 获取到注解上的数据库操作类型
+
+
+        // 2. 获取到当前被拦截的方法的参数--实体对象
+        Object[] args = joinPoint.getArgs();
+        if (args == null || args.length == 0) {
+            return;
+        }
+        Object entity = args[0];
+
+        // 3. 准备赋值数据
+        LocalDateTime now = LocalDateTime.now(); // 时间
+        Long currentId = BaseContext.getCurrentId(); // 人
+
+        // 4. 根据当前不同的操作类型,为对应的实体对象通过反射来赋值
+        if (operationType == OperationType.INSERT) {
+            // 为4个公共字段赋值
+            try {
+                entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_TIME, LocalDateTime.class).invoke(entity, now);
+                entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_USER, Long.class).invoke(entity, currentId);
+                entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class).invoke(entity, now);
+                entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class).invoke(entity, currentId);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }else if (operationType == OperationType.UPDATE) {
+            // 为两个公共字段赋值
+            try {
+                entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class).invoke(entity, now);
+                entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class).invoke(entity, currentId);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -87,12 +87,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
 
         // 设置创建时间、更新时间
-        employee.setCreateTime(LocalDateTime.now());
-        employee.setUpdateTime(LocalDateTime.now());
+        // employee.setCreateTime(LocalDateTime.now());
+        // employee.setUpdateTime(LocalDateTime.now());
 
         // 设置创建人id、更新人id
-        employee.setCreateUser(BaseContext.getCurrentId());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+        // employee.setCreateUser(BaseContext.getCurrentId());
+        // employee.setUpdateUser(BaseContext.getCurrentId());
 
         // 使用持久层map插入
         employeeMapper.insert(employee);
@@ -160,8 +160,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = new Employee();
         BeanUtils.copyProperties(employeeDTO, employee);
 
-        employee.setUpdateTime(LocalDateTime.now());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+        // employee.setUpdateTime(LocalDateTime.now());
+        // employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.update(employee);
     }

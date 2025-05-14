d45a62aeb62381610890d5504eb9db517726d1ec
HQYYK <1850115720@qq.com>
2025-05-14 12:03:26 +0800

    feat(order): 实现订单支付功能并添加支付回调接口
    - 在 OrderController 中添加订单支付接口- 在 OrderService 中实现订单支付逻辑
    - 新增 PayNotifyController 处理支付回调
    - 更新 OrderMapper 和 XML 文件以支持订单状态更新
    - 添加支付宝 SDK依赖

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,13 +2,27 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface OrderMapper {
-
     /**
      * 插入订单数据
+     * @param order
+     */
+    void insert(Orders order);
+
+    /**
+     * 修改订单信息
      * @param orders
      */
-    void insert(Orders orders);
+    void update(Orders orders);
+
+    /**
+     * 根据订单号查询订单
+     * @param orderNumber
+     * @return
+     */
+    @Select("select * from orders where number = #{orderNumber}")
+    Orders getByNumber(String orderNumber);
 }
--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -2,18 +2,19 @@ package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
 import com.sky.context.BaseContext;
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.entity.AddressBook;
 import com.sky.entity.OrderDetail;
 import com.sky.entity.Orders;
 import com.sky.entity.ShoppingCart;
 import com.sky.exception.AddressBookBusinessException;
+import com.sky.exception.OrderBusinessException;
 import com.sky.exception.ShoppingCartBusinessException;
-import com.sky.mapper.AddressBookMapper;
-import com.sky.mapper.OrderDetailMapper;
-import com.sky.mapper.OrderMapper;
-import com.sky.mapper.ShoppingCartMapper;
+import com.sky.mapper.*;
 import com.sky.service.OrderService;
+import com.sky.utils.WeChatPayUtil;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -25,89 +26,195 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-@Slf4j
+/**
+ * 订单
+ */
 @Service
+@Slf4j
 public class OrderServiceImpl implements OrderService {
-
     @Autowired
     private OrderMapper orderMapper;
     @Autowired
     private OrderDetailMapper orderDetailMapper;
-    
-    @Autowired
-    private AddressBookMapper addressBookMapper;
-    
     @Autowired
     private ShoppingCartMapper shoppingCartMapper;
     @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private AddressBookMapper addressBookMapper;
+    @Autowired
+    private WeChatPayUtil weChatPayUtil;
+    @Autowired
     private ShoppingCartImpl shoppingCartImpl;
+    @Autowired
+    private OrderService orderService;
 
     /**
      * 用户下单
+     *
      * @param ordersSubmitDTO
      * @return
      */
-    @Override
     @Transactional
     public OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO) {
-        // 1.处理各种业务异常(地址薄为空, 购物车数据为空)
+        //异常情况的处理（收货地址为空、购物车为空）
         AddressBook addressBook = addressBookMapper.getById(ordersSubmitDTO.getAddressBookId());
-        if(addressBook == null){
-            // 抛出业务异常
+        if (addressBook == null) {
             throw new AddressBookBusinessException(MessageConstant.ADDRESS_BOOK_IS_NULL);
         }
-        
-        // 查询当前用户的购物车数据
-        Long currentId = BaseContext.getCurrentId();
+
+        Long userId = BaseContext.getCurrentId();
         ShoppingCart shoppingCart = new ShoppingCart();
-        shoppingCart.setUserId(currentId);
+        shoppingCart.setUserId(userId);
+
+        //查询当前用户的购物车数据
         List<ShoppingCart> shoppingCartList = shoppingCartMapper.list(shoppingCart);
-        if(shoppingCartList == null || shoppingCartList.size() == 0){
-            // 抛出业务异常
+        if (shoppingCartList == null || shoppingCartList.size() == 0) {
             throw new ShoppingCartBusinessException(MessageConstant.SHOPPING_CART_IS_NULL);
         }
-        
-        // 2.向订单表插入1条数据
-        Orders orders = new Orders();
-        BeanUtils.copyProperties(ordersSubmitDTO, orders);
-        orders.setOrderTime(LocalDateTime.now());
-        orders.setPayStatus(Orders.UN_PAID);
-        orders.setStatus(Orders.PENDING_PAYMENT);
-        orders.setNumber(String.valueOf(System.currentTimeMillis()));
-        orders.setPhone(addressBook.getPhone());
-        orders.setConsignee(addressBook.getConsignee());
-        orders.setUserId(currentId);
 
-        String address = addressBook.getProvinceName() + addressBook.getCityName() + addressBook.getDistrictName() + addressBook.getDetail();
-        orders.setAddress(address);
+        //构造订单数据
+        Orders order = new Orders();
+        BeanUtils.copyProperties(ordersSubmitDTO,order);
+        order.setPhone(addressBook.getPhone());
+        order.setAddress(addressBook.getDetail());
+        order.setConsignee(addressBook.getConsignee());
+        order.setNumber(String.valueOf(System.currentTimeMillis()));
+        order.setUserId(userId);
+        order.setStatus(Orders.PENDING_PAYMENT);
+        order.setPayStatus(Orders.UN_PAID);
+        order.setOrderTime(LocalDateTime.now());
 
-        orderMapper.insert(orders);
-        
-        // 3. 向订单明细表插入N条数据
-        List<OrderDetail> orderDetailList = new ArrayList();
-        Long orderId = orders.getId();
+        //向订单表插入1条数据
+        orderMapper.insert(order);
+
+        //订单明细数据
+        List<OrderDetail> orderDetailList = new ArrayList<>();
         for (ShoppingCart cart : shoppingCartList) {
-            OrderDetail orderDetail = new OrderDetail(); // 订单明细
+            OrderDetail orderDetail = new OrderDetail();
             BeanUtils.copyProperties(cart, orderDetail);
-
-            orderDetail.setOrderId(orderId); // 设置当前订单明细关联的订单id.
-
+            orderDetail.setOrderId(order.getId());
             orderDetailList.add(orderDetail);
         }
 
+        //向明细表插入n条数据
         orderDetailMapper.insertBatch(orderDetailList);
-        
-        // 4. 清空当前用户的购物车
+
+        //清理购物车中的数据
         shoppingCartImpl.clean();
-        
-        // 5. 封装VO返回结果
+
+        //封装返回结果
         OrderSubmitVO orderSubmitVO = OrderSubmitVO.builder()
-                .id(orders.getId())
-                .orderNumber(orders.getNumber())
-                .orderAmount(orders.getAmount())
-                .orderTime(orders.getOrderTime())
+                .id(order.getId())
+                .orderNumber(order.getNumber())
+                .orderAmount(order.getAmount())
+                .orderTime(order.getOrderTime())
                 .build();
-        
+
         return orderSubmitVO;
+    }
+
+    /**
+     * 订单支付
+     *
+     * @param ordersPaymentDTO
+     * @return
+     */
+    public OrderPaymentVO payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception {
+        return easyPayment(ordersPaymentDTO);
+
+        // 当前登录用户id
+//        Long userId = BaseContext.getCurrentId();
+//        User user = userMapper.getById(userId);
+//
+//        //调用微信支付接口，生成预支付交易单
+//        JSONObject jsonObject = weChatPayUtil.pay(
+//                ordersPaymentDTO.getOrderNumber(), //商户订单号
+//                new BigDecimal(0.01), //支付金额，单位 元
+//                "苍穹外卖订单", //商品描述
+//                user.getOpenid() //微信用户的openid
+//        );
+//
+//        if (jsonObject.getString("code") != null && jsonObject.getString("code").equals("ORDERPAID")) {
+//            throw new OrderBusinessException("该订单已支付");
+//        }
+//
+//        OrderPaymentVO vo = jsonObject.toJavaObject(OrderPaymentVO.class);
+//        vo.setPackageStr(jsonObject.getString("package"));
+//
+//        return vo;
+    }
+
+
+
+    /**
+     * 支付成功，修改订单状态
+     *
+     * @param outTradeNo
+     */
+    public void paySuccess(String outTradeNo) {
+
+        // 根据订单号查询订单
+        Orders ordersDB = orderMapper.getByNumber(outTradeNo);
+
+        // 根据订单id更新订单的状态、支付方式、支付状态、结账时间
+        Orders orders = Orders.builder()
+                .id(ordersDB.getId())
+                .status(Orders.TO_BE_CONFIRMED)
+                .payStatus(Orders.PAID)
+                .checkoutTime(LocalDateTime.now())
+                .build();
+
+        orderMapper.update(orders);
+    }
+
+
+    private OrderPaymentVO easyPayment(OrdersPaymentDTO ordersPaymentDTO){
+        // 获取订单号
+        String orderNumber = ordersPaymentDTO.getOrderNumber();
+
+        Orders orders = orderMapper.getByNumber(orderNumber);
+
+        // 判断支付状态是否是待付款
+        if(!orders.getPayStatus().equals(Orders.UN_PAID)){
+            // 不是待付款订单,无法进行支付,抛出异常
+            throw new OrderBusinessException(MessageConstant.ORDER_STATUS_ERROR);
+        }
+
+        // 修改支付状态为已付款
+        orders.setPayStatus(Orders.PAID);
+
+        // 修改订单状态为待接单
+        orders.setStatus(Orders.TO_BE_CONFIRMED);
+
+        // 在数据库中修改
+        orderMapper.update(orders);
+
+        // 手动封装vo对象返回
+        OrderPaymentVO orderPaymentVO = OrderPaymentVO.builder()
+                .nonceStr("pay success")
+                .signType("wechat")
+                .paySign("null")
+                .timeStamp(String.valueOf(LocalDateTime.now()))
+                .packageStr("pay success")
+                .build();
+
+        return orderPaymentVO;
+
+        // 支付成功后,第一时间通知外卖商家 (来电提醒 | 出外卖单子)
+
+        // 通过WebSocket 实现服务端向客户端推送消息 (无需提醒,这就是WebSocket的优势之一)
+
+        // 客户端解析服务器推送的消息, 判断来电提送还是客户催单,并进行相应的消息提示和语音
+        // 前后端约定,服务端发送的数据格式为json,字段包括: type,orderId,content
+        // type: 1表示来电提醒,2表示催单
+        // orderId: 订单id content: 提醒内容
+        // Map<Object,Object> map = new HashMap();
+        // map.put("type", WebSocketConstant.NEW_ORDER);
+        // map.put("orderId", orders.getId());
+        // map.put("content", "订单号:" + orderNumber);
+
+        // 通过WebSocket实现来电提醒,向客户端浏览器推送消息
+        // webSocketServer.sendToAllClient(JSON.toJSONString(map));
     }
 }
